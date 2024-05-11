af17235c04d8b4555bd4d5ba531f721cc247947b
Imprarce <tribendis.nikita@mail.ru>
2024-05-11 21:18:18 +0300

    upload photo

--- src/main/kotlin/com/frencheducation/routes/Constants.kt
+++ src/main/kotlin/com/frencheducation/routes/Constants.kt
@@ -3,4 +3,7 @@ package com.frencheducation.routes
 object Constants {
     const val USER_IMAGES_PATH = "/images"
     const val BASE_URL = "http://frencheducation-imprarce.amvera.io"
+
+    //http://127.0.0.1:8080
+    //http://frencheducation-imprarce.amvera.io
 }

32bc573a2a1f1d706ce4c3daacd845e6e3e74001
Imprarce <tribendis.nikita@mail.ru>
2024-05-13 23:13:25 +0300

    update video

--- src/main/kotlin/com/frencheducation/routes/VideoRoutes.kt
+++ src/main/kotlin/com/frencheducation/routes/VideoRoutes.kt
@@ -92,7 +92,7 @@ fun Route.VideoRoutes(
         val filename = call.parameters["name"]!!
         val file = File("src\\main\\resources\\videos\\$filename")
 
-        val videoUrl = "src\\main\\resources\\videos\\$filename"
+        val videoUrl = Constants.BASE_URL + filename
         val videoPage = """
             <!DOCTYPE html>
             <html lang="en">
